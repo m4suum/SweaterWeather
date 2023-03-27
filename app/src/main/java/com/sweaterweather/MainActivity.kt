d1fc96714beec24fc59aa9e5c194aa5ebdd09137
m4suum <mag1stryod4@mail.ru>
2023-03-27 17:17:21 +0600

    УУУУУ

--- app/src/main/java/com/sweaterweather/MainActivity.kt
+++ app/src/main/java/com/sweaterweather/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.sweaterweather
 
-import android.graphics.BitmapFactory
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.graphics.drawable.toIcon
+import android.os.Handler
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.appcompat.app.AppCompatActivity
 import com.sweaterweather.databinding.ActivityMainBinding
 import com.sweaterweather.retrofit.CityWeather
 import com.sweaterweather.retrofit.RetroClient
@@ -11,12 +12,11 @@ import com.sweaterweather.retrofit.WeatherAPI
 import retrofit2.Call
 import retrofit2.Response
 import timber.log.Timber
-import java.io.IOException
-import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val weatherAPI = RetroClient.getClient().create(WeatherAPI::class.java)
+    private val handler = Handler()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -24,43 +24,62 @@ class MainActivity : AppCompatActivity() {
         Timber.plant(Timber.DebugTree())
 
         with(binding) {
-            etEnterCity.setOnClickListener {
-                val cityName = etEnterCity.text.toString()
-                weatherAPI.getWeather(cityName)
-                    .enqueue(object : retrofit2.Callback<CityWeather> {
-                        override fun onResponse(
-                            call: Call<CityWeather>, response: Response<CityWeather>
-                        ) {
-                            if (response.isSuccessful && response.body() != null) {
-                                val weatherData = response.body()
-                                tvCityName.text = cityName
-                                var temperature = weatherData?.main?.temp?.toInt()
-                                when (temperature) {
-                                    in Int.MIN_VALUE..0 -> {
-                                        tvTemperature.setTextColor(getColor(R.color.dark_blue))
-                                    }
-                                    in 1..9 -> {
-                                        tvTemperature.setTextColor(getColor(R.color.green))
-                                    }
-                                    in 10..19 -> {
-                                        tvTemperature.setTextColor(getColor(R.color.yellow))
-                                    }
-                                    in 20..Int.MAX_VALUE -> {
-                                        tvTemperature.setTextColor(getColor(R.color.red))
+            etEnterCity.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    before: Int,
+                    count: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    handler.removeCallbacksAndMessages(null)
+                    handler.postDelayed({
+                        val cityName = etEnterCity.text.toString()
+                        weatherAPI.getWeather(cityName)
+                            .enqueue(object : retrofit2.Callback<CityWeather> {
+                                override fun onResponse(
+                                    call: Call<CityWeather>, response: Response<CityWeather>
+                                ) {
+                                    if (response.isSuccessful && response.body() != null) {
+                                        val weatherData = response.body()
+                                        tvCityName.text = cityName
+                                        var temperature = weatherData?.main?.temp?.toInt()
+                                        when (temperature) {
+                                            in Int.MIN_VALUE..0 -> {
+                                                tvTemperature.setTextColor(getColor(R.color.dark_blue))
+                                            }
+                                            in 1..9 -> {
+                                                tvTemperature.setTextColor(getColor(R.color.green))
+                                            }
+                                            in 10..19 -> {
+                                                tvTemperature.setTextColor(getColor(R.color.yellow))
+                                            }
+                                            in 20..Int.MAX_VALUE -> {
+                                                tvTemperature.setTextColor(getColor(R.color.red))
+                                            }
+                                        }
+                                        tvTemperature.text =
+                                            "${weatherData?.main?.temp.toString()} °C"
+                                        tvDescription.text =
+                                            weatherData?.weather?.get(0)?.description.toString()
+                                        Timber.i("______________${tvTemperature.text}")
+
                                     }
                                 }
-                                tvTemperature.text = "${weatherData?.main?.temp.toString()} °C"
-                                tvDescription.text =
-                                    weatherData?.weather?.get(0)?.description.toString()
-                                Timber.i("______________${tvTemperature.text}")
-                            }
-                        }
 
-                        override fun onFailure(call: Call<CityWeather>, t: Throwable) {
-                            Timber.e("---ERROR -> ${t.message.toString()}")
-                        }
-                    })
-            }
+                                override fun onFailure(call: Call<CityWeather>, t: Throwable) {
+                                    Timber.e("---ERROR -> ${t.message.toString()}")
+                                }
+                            })
+                    }, 2000)
+                }
+            })
         }
     }
 }
+
